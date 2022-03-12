5691969bfc987cd26d2bbe9fe6dff81131b3d37e
Reactgithub2022 <sundareswar.bj@gmail.com>
2022-03-12 22:09:18 +0530

    new heading

--- src/Sam.js
+++ src/Sam.js
@@ -3,7 +3,8 @@ import React, { Component } from 'react'
 class Sam extends Component {
   render() {
     return (
-      <div><h1>Sam</h1></div>
+      <div><h1>Sam</h1>
+      <h1>prabhu</h1></div>
     )
   }
 }
