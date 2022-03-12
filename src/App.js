b8d008f47c08f29234759e566087b65b1293dcfe
Reactgithub2022 <sundareswar.bj@gmail.com>
2022-03-12 22:05:55 +0530

    new Sam component

--- src/App.js
+++ src/App.js
@@ -5,16 +5,18 @@ import './App.css';
 import Home from './Home';
 import About from './About';
 import Contact from './Contact';
+import Sam from './Sam';
 
 function App() {
   return (
     <div className="App">
       {/* <Counter/> */}
-      <Routes>
+      {/* <Routes>
       <Route path="/" element={Home}/>
       <Route path="/about" element={About}/>
       <Route path="/contact" element={Contact}/>
-      </Routes>
+      </Routes> */}
+      <Sam/>
     </div>
   );
 }
---
+++ src/Sam.js
@@ -0,0 +1,11 @@
+import React, { Component } from 'react'
+
+class Sam extends Component {
+  render() {
+    return (
+      <div><h1>Sam</h1></div>
+    )
+  }
+}
+
+export default Sam
